76591072b7f0668f8593bcf9f076ef0f9fba188a
hakanozoglu <hakan.ozoglu@grapecity.com>
2022-02-28 15:07:21 +0300

    changes on base

--- src/app/controller/auth/methods/user.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler } from "express";
-
-export const user: RequestHandler = async (request, response) => {
-  try {
-    response.status(200).send(request.authUser);
-  } catch {
-    response.status(500).send("Server error");
-  }
-}
