01ea0538bb2b6a2acc4d223074deeb8febf74891
hakanozoglu <hakan.ozoglu@grapecity.com>
2022-03-02 21:44:19 +0300

    fix query validation

--- src/controller/status/methods/update/schema.ts
+++ src/controller/status/methods/update/schema.ts
@@ -1,8 +1,8 @@
 import { zod } from "@ooic/core";
 export const body = zod.object({
-  title: zod.string(),
-  categoryId: zod.number(),
-  color: zod.string(),
+  title: zod.string().optional(),
+  categoryId: zod.number().optional(),
+  color: zod.string().optional(),
 });
 export const params = zod.object({
   id: zod.string().regex(/^\d+$/).transform(Number)
